e5697305766860dc0d358130aff1ad35ae2aea61
Ash Connell <hello@ashconnell.com>
2020-08-21 12:38:13 +1000

    [hecs] prevent registering duplicate components

--- packages/hecs/src/ComponentManager.js
+++ packages/hecs/src/ComponentManager.js
@@ -7,6 +7,9 @@ export class ComponentManager {
   }
 
   register(Component) {
+    if (this.componentsByName[Component.name]) {
+      throw new Error(`hecs: component already registered '${Component.name}'`)
+    }
     Component.id = this.lastComponentId++
     this.componentsByName[Component.name] = Component
     this.count++
