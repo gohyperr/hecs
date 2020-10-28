af7c34c5a5ff5523c4d23ff47d00c9df775dfba7
Duane Johnson <duane.johnson@gmail.com>
2020-10-27 18:35:15 -0600

    Use @babel/plugin-transform-runtime

--- packages/hecs-plugin-three/babel.config.js
+++ packages/hecs-plugin-three/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = api => {
   api.cache(true)
   return {
-    presets: [['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]],
-    plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+    presets: ['@babel/preset-env'],
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      '@babel/plugin-transform-runtime'
+    ],
   }
 }
