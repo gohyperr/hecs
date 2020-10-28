88632017efbcd8872225ca117227f4a583a36a15
Duane Johnson <duane.johnson@gmail.com>
2020-10-28 07:25:25 -0600

    Add transform-runtime, update babel

--- packages/hecs-plugin-three/babel.config.js
+++ packages/hecs-plugin-three/babel.config.js
@@ -2,6 +2,9 @@ module.exports = api => {
   api.cache(true)
   return {
     presets: ['@babel/preset-env'],
-    plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      '@babel/plugin-transform-runtime'
+    ],
   }
 }
