4ae3e8f89feafad03c58f4e3946224daab744c54
Duane Johnson <duane.johnson@gmail.com>
2020-10-30 06:34:51 -0600

    Add hecs-plugin-composable

---
+++ packages/hecs-plugin-composable/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = api => {
+  api.cache(true)
+  return {
+    presets: ['@babel/preset-env'],
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      '@babel/plugin-transform-runtime'
+    ],
+  }
+}
---
+++ packages/hecs-plugin-composable/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = env => {
+  const config = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'hecs-plugin-composable.js',
+      library: {
+        root: 'HecsPluginComposable',
+        amd: 'hecs-plugin-composable',
+        commonjs: 'hecs-plugin-composable',
+      },
+      libraryTarget: 'umd',
+      globalObject: 'this',
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          terserOptions: {
+            keep_classnames: true,
+            keep_fnames: true,
+          },
+        }),
+      ],
+    },
+    devtool: 'source-map',
+    node: { fs: 'empty' },
+    plugins: [new CleanWebpackPlugin()],
+    externals: [
+      {
+        hecs: {
+          root: 'HECS',
+          amd: 'hecs',
+          commonjs: 'hecs',
+          commonjs2: 'hecs',
+        },
+        'hecs-plugin-core': {
+          root: 'HecsPluginCore',
+          amd: 'hecs-plugin-core',
+          commonjs: 'hecs-plugin-core',
+          commonjs2: 'hecs-plugin-core',
+        },
+      },
+      /^three\/examples/,
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+  }
+  if (isProduction) {
+    config.mode = 'production'
+    config.devtool = false
+  }
+  return config
+}
